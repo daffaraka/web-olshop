08435fd7e615cbc5fb780a39d2eb9f2c60a7926f
lintanganlr <lintangangraini3@gmail.com>
2023-07-15 15:29:05 +0700

    route dan controlller

--- routes/web.php
+++ routes/web.php
@@ -40,85 +40,96 @@ Route::get('/register', function () {
     return view("register");
 });
 
-Route::get('Pemesanan', [ProductController::class, 'index'])->name('Pemesanan.index');
-Route::get('Pemesanan/create', [ProductController::class, 'create'])->name('Pemesanan.create');
-Route::post('Pemesanan', [ProductController::class, 'store'])->name('Pemesanan.store');
-Route::get('Pemesanan/{id}/edit', [ProductController::class, 'edit'])->name('Pemesanan.edit');
-Route::put('Pemesanan/{id}', [ProductController::class, 'update'])->name('Pemesanan.update');
-Route::delete('Pemesanan/{id}', [ProductController::class, 'destroy'])->name('Pemesanan.destroy');
+Route::resource('Pemesanan', PemesananController::Class);
+Route::get('Pemesanan', [PemesananController::class, 'index'])->name('Pemesanan.index');
+Route::get('Pemesanan/create', [PemesananController::class, 'create'])->name('Pemesanan.create');
+Route::post('Pemesanan', [PemesananController::class, 'store'])->name('Pemesanan.store');
+Route::get('Pemesanan/{id}/edit', [PemesananController::class, 'edit'])->name('Pemesanan.edit');
+Route::put('Pemesanan/{id}', [PemesananController::class, 'update'])->name('Pemesanan.update');
+Route::delete('Pemesanan/{id}', [PemesananController::class, 'destroy'])->name('Pemesanan.destroy');
 
-Route::get('Pembayaran', [ProductController::class, 'index'])->name('Pembayaran.index');
-Route::get('Pembayaran/create', [ProductController::class, 'create'])->name('Pembayaran.create');
-Route::post('Pembayaran', [ProductController::class, 'store'])->name('Pembayaran.store');
-Route::get('Pembayaran/{id}/edit', [ProductController::class, 'edit'])->name('Pembayaran.edit');
-Route::put('Pembayaran/{id}', [ProductController::class, 'update'])->name('Pembayaran.update');
-Route::delete('Pembayaran/{id}', [ProductController::class, 'destroy'])->name('Pembayaran.destroy');
+Route::resource('Pembayaran', PembayaranController::Class);
+Route::get('Pembayaran', [PembayaranController::class, 'index'])->name('Pembayaran.index');
+Route::get('Pembayaran/create', [PembayaranController::class, 'create'])->name('Pembayaran.create');
+Route::post('Pembayaran', [PembayaranController::class, 'store'])->name('Pembayaran.store');
+Route::get('Pembayaran/{id}/edit', [PembayaranController::class, 'edit'])->name('Pembayaran.edit');
+Route::put('Pembayaran/{id}', [PembayaranController::class, 'update'])->name('Pembayaran.update');
+Route::delete('Pembayaran/{id}', [PembayaranController::class, 'destroy'])->name('Pembayaran.destroy');
 
+Route::resource('MetodePembayaran', MetodePembayaranController::Class);
+Route::get('MetodePembayaran', [MetodePembayaranController::class, 'index'])->name('MetodePembayaran.index');
+Route::get('MetodePembayaran/create', [MetodePembayaranController::class, 'create'])->name('MetodePembayaran.create');
+Route::post('MetodePembayaran', [MetodePembayaranController::class, 'store'])->name('MetodePembayaran.store');
+Route::get('MetodePembayaran/{id}/edit', [MetodePembayaranController::class, 'edit'])->name('MetodePembayaran.edit');
+Route::put('MetodePembayaran/{id}', [MetodePembayaranController::class, 'update'])->name('MetodePembayaran.update');
+Route::delete('MetodePembayaran/{id}', [MetodePembayaranController::class, 'destroy'])->name('MetodePembayaran.destroy');
 
-Route::get('MetodePembayaran', [ProductController::class, 'index'])->name('MetodePembayaran.index');
-Route::get('MetodePembayaran/create', [ProductController::class, 'create'])->name('MetodePembayaran.create');
-Route::post('MetodePembayaran', [ProductController::class, 'store'])->name('MetodePembayaran.store');
-Route::get('MetodePembayaran/{id}/edit', [ProductController::class, 'edit'])->name('MetodePembayaran.edit');
-Route::put('MetodePembayaran/{id}', [ProductController::class, 'update'])->name('MetodePembayaran.update');
-Route::delete('MetodePembayaran/{id}', [ProductController::class, 'destroy'])->name('MetodePembayaran.destroy');
+Route::resource('Tunai', TunaiController::Class);
+Route::get('Tunai', [TunaiController::class, 'index'])->name('Tunai.index');
+Route::get('Tunai/create', [TunaiController::class, 'create'])->name('Tunai.create');
+Route::post('Tunai', [TunaiController::class, 'store'])->name('Tunai.store');
+Route::get('Tunai/{id}/edit', [TunaiController::class, 'edit'])->name('Tunai.edit');
+Route::put('Tunai/{id}', [TunaiController::class, 'update'])->name('Tunai.update');
+Route::delete('Tunai/{id}', [TunaiController::class, 'destroy'])->name('Tunai.destroy');
 
+Route::resource('Transfer', TransferController::Class);
+Route::get('Transfer', [TransferController::class, 'index'])->name('Transfer.index');
+Route::get('Transfer/create', [TransferController::class, 'create'])->name('Transfer.create');
+Route::post('Transfer', [TransferController::class, 'store'])->name('Transfer.store');
+Route::get('Transfer/{id}/edit', [TransferController::class, 'edit'])->name('Transfer.edit');
+Route::put('Transfer/{id}', [TransferController::class, 'update'])->name('Transfer.update');
+Route::delete('Transfer/{id}', [TransferController::class, 'destroy'])->name('Transfer.destroy');
 
-Route::get('Tunai', [ProductController::class, 'index'])->name('Tunai.index');
-Route::get('Tunai/create', [ProductController::class, 'create'])->name('Tunai.create');
-Route::post('Tunai', [ProductController::class, 'store'])->name('Tunai.store');
-Route::get('Tunai/{id}/edit', [ProductController::class, 'edit'])->name('Tunai.edit');
-Route::put('Tunai/{id}', [ProductController::class, 'update'])->name('Tunai.update');
-Route::delete('Tunai/{id}', [ProductController::class, 'destroy'])->name('Tunai.destroy');
+Route::resource('DompetElektronik', DompetElektronikController::Class);
+Route::get('DompetElektronik', [DompetElektronikController::class, 'index'])->name('DompetElektronik.index');
+Route::get('DompetElektronik/create', [DompetElektronikController::class, 'create'])->name('DompetElektronik.create');
+Route::post('DompetElektronik', [DompetElektronikController::class, 'store'])->name('DompetElektronik.store');
+Route::get('DompetElektronik/{id}/edit', [DompetElektronikController::class, 'edit'])->name('DompetElektronik.edit');
+Route::put('DompetElektronik/{id}', [DompetElektronikController::class, 'update'])->name('DompetElektronik.update');
+Route::delete('DompetElektronik/{id}', [DompetElektronikController::class, 'destroy'])->name('DompetElektronik.destroy');
 
-Route::get('Transfer', [ProductController::class, 'index'])->name('Transfer.index');
-Route::get('Transfer/create', [ProductController::class, 'create'])->name('Transfer.create');
-Route::post('Transfer', [ProductController::class, 'store'])->name('Transfer.store');
-Route::get('Transfer/{id}/edit', [ProductController::class, 'edit'])->name('Transfer.edit');
-Route::put('Transfer/{id}', [ProductController::class, 'update'])->name('Transfer.update');
-Route::delete('Transfer/{id}', [ProductController::class, 'destroy'])->name('Transfer.destroy');
+Route::resource('Barang', BarangController::Class);
+Route::get('Barang', [BarangController::class, 'index'])->name('Barang.index');
+Route::get('Barang/create', [BarangController::class, 'create'])->name('Barang.create');
+Route::post('Barang', [BarangController::class, 'store'])->name('Barang.store');
+Route::get('Barang/{id}/edit', [BarangController::class, 'edit'])->name('Barang.edit');
+Route::put('Barang/{id}', [BarangController::class, 'update'])->name('Barang.update');
+Route::delete('Barang/{id}', [BarangController::class, 'destroy'])->name('Barang.destroy');
 
-Route::get('DompetElektronik', [ProductController::class, 'index'])->name('DompetElektronik.index');
-Route::get('DompetElektronik/create', [ProductController::class, 'create'])->name('DompetElektronik.create');
-Route::post('DompetElektronik', [ProductController::class, 'store'])->name('DompetElektronik.store');
-Route::get('DompetElektronik/{id}/edit', [ProductController::class, 'edit'])->name('DompetElektronik.edit');
-Route::put('DompetElektronik/{id}', [ProductController::class, 'update'])->name('DompetElektronik.update');
-Route::delete('DompetElektronik/{id}', [ProductController::class, 'destroy'])->name('DompetElektronik.destroy');
+Route::resource('HistoriPemeasanan', HistoriPemeasananController::Class);
+Route::get('HistoriPemeasanan', [HistoriPemeasananController::class, 'index'])->name('HistoriPemesanan.index');
+Route::get('HistoriPemesanan/create', [HistoriPemeasananController::class, 'create'])->name('HistoriPemesanan.create');
+Route::post('HistoriPemesanan', [HistoriPemeasananController::class, 'store'])->name('HistoriPemesanan.store');
+Route::get('HistoriPemesanan/{id}/edit', [HistoriPemeasananController::class, 'edit'])->name('HistoriPemesanan.edit');
+Route::put('HistoriPemesanan/{id}', [HistoriPemeasananController::class, 'update'])->name('HistoriPemesanan.update');
+Route::delete('HistoriPemesanan/{id}', [HistoriPemeasananController::class, 'destroy'])->name('HistoriPemesanan.destroy');
 
-Route::get('Barang', [ProductController::class, 'index'])->name('Barang.index');
-Route::get('Barang/create', [ProductController::class, 'create'])->name('Barang.create');
-Route::post('Barang', [ProductController::class, 'store'])->name('Barang.store');
-Route::get('Barang/{id}/edit', [ProductController::class, 'edit'])->name('Barang.edit');
-Route::put('Barang/{id}', [ProductController::class, 'update'])->name('Barang.update');
-Route::delete('Barang/{id}', [ProductController::class, 'destroy'])->name('Barang.destroy');
+Route::resource('KategoriBarang', KategoriBarangController::Class);
+Route::get('KategoriBarang', [KategoriBarangController::class, 'index'])->name('KategoriBarang.index');
+Route::get('KategoriBarang/create', [KategoriBarangController::class, 'create'])->name('KategoriBarang.create');
+Route::post('KategoriBarang', [KategoriBarangController::class, 'store'])->name('KategoriBarang.store');
+Route::get('KategoriBarang/{id}/edit', [KategoriBarangController::class, 'edit'])->name('TunaKategoriBarangi.edit');
+Route::put('KategoriBarang/{id}', [KategoriBarangController::class, 'update'])->name('KategoriBarang.update');
+Route::delete('KategoriBarang/{id}', [KategoriBarangController::class, 'destroy'])->name('KategoriBarang.destroy');
 
-Route::get('HistoriPemesanan', [ProductController::class, 'index'])->name('HistoriPemesanan.index');
-Route::get('HistoriPemesanan/create', [ProductController::class, 'create'])->name('HistoriPemesanan.create');
-Route::post('HistoriPemesanan', [ProductController::class, 'store'])->name('HistoriPemesanan.store');
-Route::get('HistoriPemesanan/{id}/edit', [ProductController::class, 'edit'])->name('HistoriPemesanan.edit');
-Route::put('HistoriPemesanan/{id}', [ProductController::class, 'update'])->name('HistoriPemesanan.update');
-Route::delete('HistoriPemesanan/{id}', [ProductController::class, 'destroy'])->name('HistoriPemesanan.destroy');
+Route::resource('StokBarang', StokBarangController::Class);
+Route::get('StokBarang', [StokBarangController::class, 'index'])->name('StokBarang.index');
+Route::get('StokBarang/create', [StokBarangController::class, 'create'])->name('StokBarang.create');
+Route::post('StokBarang', [StokBarangController::class, 'store'])->name('StokBarang.store');
+Route::get('StokBarang/{id}/edit', [StokBarangController::class, 'edit'])->name('StokBarang.edit');
+Route::put('StokBarang/{id}', [StokBarangController::class, 'update'])->name('StokBarang.update');
+Route::delete('StokBarang/{id}', [StokBarangController::class, 'destroy'])->name('StokBarang.destroy');
 
-Route::get('KategoriBarang', [ProductController::class, 'index'])->name('KategoriBarang.index');
-Route::get('KategoriBarang/create', [ProductController::class, 'create'])->name('KategoriBarang.create');
-Route::post('KategoriBarang', [ProductController::class, 'store'])->name('KategoriBarang.store');
-Route::get('KategoriBarang/{id}/edit', [ProductController::class, 'edit'])->name('TunaKategoriBarangi.edit');
-Route::put('KategoriBarang/{id}', [ProductController::class, 'update'])->name('KategoriBarang.update');
-Route::delete('KategoriBarang/{id}', [ProductController::class, 'destroy'])->name('KategoriBarang.destroy');
-
-Route::get('StokBarang', [ProductController::class, 'index'])->name('StokBarang.index');
-Route::get('StokBarang/create', [ProductController::class, 'create'])->name('StokBarang.create');
-Route::post('StokBarang', [ProductController::class, 'store'])->name('StokBarang.store');
-Route::get('StokBarang/{id}/edit', [ProductController::class, 'edit'])->name('StokBarang.edit');
-Route::put('StokBarang/{id}', [ProductController::class, 'update'])->name('StokBarang.update');
-Route::delete('StokBarang/{id}', [ProductController::class, 'destroy'])->name('StokBarang.destroy');
-
-Route::get('Role', [ProductController::class, 'index'])->name('Role.index');
-Route::get('Role/create', [ProductController::class, 'create'])->name('Role.create');
-Route::post('Role', [ProductController::class, 'store'])->name('Role.store');
-Route::get('Role/{id}/edit', [ProductController::class, 'edit'])->name('Role.edit');
-Route::put('Role/{id}', [ProductController::class, 'update'])->name('Role.update');
-Route::delete('Role/{id}', [ProductController::class, 'destroy'])->name('Role.destroy');
+Route::resource('Role', RoleController::Class);
+Route::get('Role', [RoleController::class, 'index'])->name('Role.index');
+Route::get('Role/create', [RoleController::class, 'create'])->name('Role.create');
+Route::post('Role', [RoleController::class, 'store'])->name('Role.store');
+Route::get('Role/{id}/edit', [RoleController::class, 'edit'])->name('Role.edit');
+Route::put('Role/{id}', [RoleController::class, 'update'])->name('Role.update');
+Route::delete('Role/{id}', [RoleController::class, 'destroy'])->name('Role.destroy');
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+?>
