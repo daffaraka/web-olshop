aef9ca11142b563ccfa035db9a7fda04e3054dcd
Daffa Raka <daffarakals@gmail.com>
2023-07-20 14:26:12 +0700

    Seeder

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Carbon\Carbon;
 use App\Models\Barang;
+use App\Models\Pembayaran;
 use App\Models\Pemesanan;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -22,13 +23,14 @@ class HomeController extends Controller
         ]);
     }
 
-    public function show ($id) {
+    public function show($id)
+    {
         $barang = Barang::find($id);
-
     }
 
 
-    public function addToCart(Request $request) {
+    public function addToCart(Request $request)
+    {
 
 
         $barang = Barang::find($request->id_barang);
@@ -42,19 +44,45 @@ class HomeController extends Controller
 
 
         return redirect()->route('cart');
-
     }
 
-    public function cart() {
+    public function cart()
+    {
 
         $user_id = Auth::user()->id;
-        $cart = Pemesanan::where('user_id',$user_id)->where('status_pemesanan','pending')->get();
+        $cart = Pemesanan::where('user_id', $user_id)->where('status_pemesanan', 'pending')->get();
 
-        return view('cart',compact('cart'));
+        return view('cart', compact('cart'));
     }
 
 
-    public function detailPesanan($id) {
+    public function pembayaran($id)
+    {
+        $pemesanan = Pemesanan::find($id);
+
+        return view('home.pembayaran');
+    }
+
+
+    public function pay($id)
+    {
+        $pemesanan = Pemesanan::find($id);
+        Pembayaran::create([
+            'id_pemesanan' => $pemesanan->id,
+            'tanggal_pembayaran' => Carbon::now(),
+            'total_pembayaran' => $pemesanan->total_pemesanan,
+            'status_pembayaran' => 'Selesai'
+        ]);
+
+
+        return redirect()->route('cart');
+
+    }
+
+
+
+    public function detailPesanan($id)
+    {
         $pesanan = Pemesanan::find($id);
     }
 }
